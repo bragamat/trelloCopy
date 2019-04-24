2a7cc94068ee74d342451b1e308c4e49771bbbb4
Mateus Braga <mateuscbraga@gmail.com>
2019-04-24 10:59:08 -0300

    adding more test

--- src/__tests__/components/Modal.spec.js
+++ src/__tests__/components/Modal.spec.js
@@ -3,13 +3,36 @@ import { mount } from 'enzyme'
 
 import Modal from '../../components/Modal'
 
-it('should render a Modal', () => {
-  const wrapper = mount(<Modal children={<h1></h1>} />)
-  expect(wrapper.find('div').exists()).toBe(true)
+describe('Modal Stuff', () => {
+  let spy = spyConsole();
+  it('should render a Modal', () => {
+    const wrapper = mount(<Modal children={<h1></h1>} />)
+    expect(wrapper.find('div').exists()).toBe(true)
+  })
+  it('should render a Modal with the content passed within props as children', () => {
+    const wrapper = mount(<Modal children={<h1></h1>} />)
+    expect(wrapper.contains(<h1></h1>)).toBe(true)
+  })
+
+  it('Should warning about the propTypes', () => {
+    mount(<Modal />)
+    expect(console.error).toHaveBeenCalled();
+  })
+
 })
 
 
-it('should render a Modal with an h1 tag as childre', () => {
-  const wrapper = mount(<Modal children={<h1></h1>} />)
-  expect(wrapper.contains(<h1></h1>)).toBe(true)
-})
+
+
+
+function spyConsole() {
+  // https://github.com/facebook/react/issues/7047
+  let spy = {};
+  beforeAll(() => {
+      spy.console = jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+  afterAll(() => {
+      spy.console.mockRestore();
+  });
+  return spy;
+}
